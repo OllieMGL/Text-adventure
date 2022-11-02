b44e4dbf2237efd13aaf9d307793889d9006c062
ollie <olliejg18@gmail.com>
2022-11-02 20:40:23 +0000

    name change

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def handleUserInput(user_input):
         player.current_location.setCanAttack(True)
 
 
-print("Welcome to Skyrim but easy...")
+print("Welcome to The Knights of the Square Table...")
 print(player.current_location.description)
 
 while player.health > 0:
